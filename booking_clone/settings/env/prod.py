b2a63637cb3c1483a6226d9c31cb4c40ff37cda7
5ar1ja <ralimkhanov@icloud.com>
2026-05-16 15:16:17 +0500

    deploy version

--- booking_clone/settings/env/prod.py
+++ booking_clone/settings/env/prod.py
@@ -1,19 +1,25 @@
 import os
+import dj_database_url
 
 # Project modules
 from settings.base import *  # noqa: F403
 
 
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",") + ["*"]
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+# Insert WhiteNoiseMiddleware after SecurityMiddleware
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB', 'booking_clone'),
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
